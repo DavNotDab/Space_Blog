7ed46e36fff1b60ff6d11c73af2d86d91d5ec263
David Ballesteros Ortiz <david13ballesteros@gmail.com>
2023-02-03 14:41:33 +0100

    Reworked directory configuration

--- scripts/videoLoad.js
+++ scripts/videoLoad.js
@@ -1,10 +1,10 @@
 
 function loadVideo() {
     let video = document.getElementById("backgroundVideo");
-    if ( screen.width > 800 ) { video.src = "../images/backgroundVideo.mp4"; }
-    if ( screen.width <= 800 ) { video.src = "../images/backgroundVideo2.mp4"; }
-    if ( screen.width <= 600 ) { video.src = "../images/backgroundVideo3.mp4"; }
-    if ( screen.width <= 450 ) { video.src = "../images/backgroundVideo4.mp4"; }
+    if ( screen.width > 800 ) { video.src = "../media/video/bgVideo-1080.mp4"; }
+    if ( screen.width <= 800 ) { video.src = "../media/video/bgVideo-720.mp4"; }
+    if ( screen.width <= 600 ) { video.src = "../media/video/bgVideo-480.mp4"; }
+    if ( screen.width <= 450 ) { video.src = "../media/video/bgVideo-360.mp4"; }
 
     video.load();
 }
